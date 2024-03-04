fa4d68e1f8aa4d8936a3ea55eff9f7e4407b4883
lzbGEM <161556222+lzbGEM@users.noreply.github.com>
2024-03-04 23:26:25 +0800

    Add files via upload

---
+++ recursive.py
@@ -0,0 +1,12 @@
+"""
+a=4
+a=a*2+3
+calculate 5 times needs to use for
+"""
+a=4
+print(a)
+for i in range(1,6):
+    a=a*2+3
+    print(a)
+
+
---
+++ variables.py
@@ -0,0 +1,12 @@
+a=40
+b=36
+c=30
+d=a-b
+e=b-c
+if d>e:
+    print("running training is better")
+else:
+    print("running and strength training is better")
+#running and strength training is better
+
+#True
