8d03aa8ea32727ce3e887c529315396fed85c540
lzbGEM <161556222+lzbGEM@users.noreply.github.com>
2024-03-05 00:05:52 +0800

    Update culture_density.py

--- culture_density.py
+++ culture_density.py
@@ -1,6 +1,6 @@
 """
 The initial density is 0.05
-a=0  is used to calculate the day
+a=1 is used to calculate the day
 while density<100:
      density=density*2
      a=a+1
@@ -12,10 +12,11 @@ When density exceeds 100, a is the number of days calculated
 
 
 density=0.05
-a=0
+a=1
 while density<0.9:
     density*=2
     a+=1
+    print(f'the cell density on day {a} is {density})
 print(f"Cell density does not exceed 0.9 until day {a} and will exceed from day {a+1} onwards")
 
 
