b6716ccf54d9a717e886c79482c422c657339e30
lzbGEM <161556222+lzbGEM@users.noreply.github.com>
2024-03-05 16:49:28 +0800

    Update mystery_code.py

--- mystery_code.py
+++ mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer:Output the sum of 100 random integers between 1 and 10,and the variable names in this code are typed incorrectly
+# Answer:Output the sum of 100 random integers between 1 and 10
 
 # Import libraries
 # randint allows drawing a random number,
@@ -11,7 +11,7 @@ from random import randint
 from math import ceil#Import ceil in the math module
 
 progress=0#Assign the initial values of both variables to 0
-total_randonumbem_r=0
+total_random_numbem_r=0
 while progress<100:#Make a judgment in the while loop and enter the loop when progress is less than 100
 	progress+=1#progress=progress+1,this loop can be done 100 times
 	n = randint(1,10)#n is a random integer ranging from 1 to 10
